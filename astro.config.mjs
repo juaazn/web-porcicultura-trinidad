6abad4458ba80f811fc9cf8044a11b21ff590de7
chuans <juaazn.05@gmail.com>
2023-08-24 21:02:46 -0500

    Config server cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,7 @@
 import { defineConfig } from 'astro/config'
 import cloudflare from '@astrojs/cloudflare'
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  adapter: cloudflare({ mode: 'advanced' }),
-
-  vite: {
-    build : {
-      minify: false
-    }
-  }
+  output: 'server',
+  adapter: cloudflare({ mode: 'advanced' })
 })
